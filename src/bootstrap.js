9f3ccb58c35fdcca02dcebe7a7df584695d4c87b
Destinee Muse <destineemuse20@yahoo.com>
2022-05-11 22:45:04 -0500

    updated styles, added store.js and updated app.js and NavBar.js with new links

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import App from "./components/app";
 import About from "./components/about";
+import Store from "./components/store";
+import Contact from "./components/Contact";
 
 import "./style/main.scss";
 import NavBar from "./components/NavBar";
-import Contact from "./components/Contact";
+
 
 function main() {
   ReactDOM.render(
@@ -21,6 +23,9 @@ function main() {
         </Route>
         <Route path="/contact">
           <Contact />
+        </Route>
+        <Route path="/store">
+          <Store />
         </Route>
       </Switch>
     </BrowserRouter>,
--- src/components/app.js
+++ src/components/app.js
@@ -5,9 +5,10 @@ export default class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1>DevCamp React Starter</h1>
+        <h1>Destinee's Page</h1>
         <h2>React + React Router</h2>
       </div>
     );
   }
 }
+
